350cd2f245e4325ff16edf28869621eaad438880
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-07-12 16:50:22 -0300

    FEAT(API): Create API and state management for Avaliacoes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <script setup>
-import { useCursoStore } from '@/stores/cursosStore';
-import { ref, onMounted } from 'vue';
+import { useAvaliacaoStore } from '@/stores/avaliacoesStore';
+import { onMounted } from 'vue';
 
-const useCurso = useCursoStore()
-const newCurso = {id: 1, nome: 'Informática', carga_horaria: 100, sigla: 'Info', nivel_curso: 'tecn'};
-const data = ref(null);
+const useAvaliacao = useAvaliacaoStore();
+const newAvaliation = {avaliador: 1, equipe: 2, sigla: 1, nota: 10.0};
 
 onMounted(async () => {
-    let response1 = await useCurso.deleteCurso(1);
-    // let response2 = await useCurso.deleteCurso(1);
+    let response1 = await useAvaliacao.createAvaliacao(newAvaliation);
+    let response2 = await useAvaliacao.getAvaliacoes();
 
-    console.log('Feito!', response1);
-})
+    console.log('Feito!', response1, response2);
+});
 </script>
 
 <template>
