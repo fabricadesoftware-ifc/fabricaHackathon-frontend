94908ea8ca281bb47dd3b8b21a7331adb5a144d7
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-07-17 20:47:12 -0300

    FEAT(API): Create API and state management for Teams [Refs: #12]

---
+++ src/services/teamServices.js
@@ -0,0 +1,26 @@
+import api from '@/plugins/axios';
+
+class TeamService {
+    async getTeams() {
+        const { data } = await api.get('/teams/');
+        return data;
+    }
+    async getTeam(id) {
+        const { data } = await api.get(`/teams/${id}/`);
+        return data;
+    }
+    async createTeam(team) {
+        const { data } = await api.post('/teams/', team);
+        return data;
+    }
+    async updateTeam(team) {
+        const { data } = await api.patch(`/teams/${team.id}/`, team);
+        return data;
+    }
+    async deleteTeam(id) {
+        const { data } = await api.delete(`/teams/${id}/`);
+        return data;
+    }
+}
+
+export default new TeamService();
