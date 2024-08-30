5ed22cba388954b15ad267de7e69c53486d573f6
AnthonyLoche <anthonylocheifc@gmail.com>
2024-08-30 16:02:26 -0300

    Feat(Layout): Create Footer and Header

---
+++ src/components/global/footer/FooterComp.vue
@@ -0,0 +1,39 @@
+<script setup>
+import SocialsComp from './SocialsComp.vue';
+import LinksComp from './LinksComp.vue';
+</script>
+
+
+<template>
+    <footer>
+        <div class="logo">
+            <img src="/public/logoHackaton.png" alt="">
+        </div>
+        <SocialsComp />
+        <LinksComp />
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    background: radial-gradient(97.57% 210.75% at 0.9% 2.98%, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0) 100%);
+    width: 100%;
+    padding: 1rem;
+    height: 430px;
+    display: grid;
+    grid-template-columns: 1fr 0.5fr 1fr;
+}
+
+img {
+    width: 40%;
+    margin: 0 auto;
+}
+
+.logo {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+}
+
+</style>
