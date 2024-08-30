5987569fc8b0d1569cc892cb7b0c0f8267ac2e07
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-08-30 17:08:52 -0300

    CHORE: Start AuthPage

--- src/pages/avaliator/AuthAvaliatorView.vue
+++ src/pages/avaliator/AuthAvaliatorView.vue
@@ -1,12 +1,64 @@
 <script setup>
-import TextInput from '@/components/global/input/TextInput.vue';
-import PasswordInput from '@/components/global/input/PasswordInput.vue';
-import SearchInput from '@/components/global/input/SearchInput.vue';
-import SelectInput from '@/components/global/input/SelectInput.vue';
-
+import { ref } from 'vue';
 </script>
 
 
 <template>
-    <TextInput label="Usuário"></TextInput>
+    <div class="main-container">
+        <div class="container-auth">
+            <div class="container-logo">
+                <img src="@/assets/global/logo.png" alt="">
+            </div>
+
+            <div class="container-form">
+                <form>
+                    <h1 class="title-form">Login</h1>
+                    <div class="inputContainer">
+                        <label for="inputUser">Usuário</label>
+                        <input type="text" id="inputUser">
+                    </div>
+                    <div class="inputContainer">
+                        <label for="inputPassword">Senha</label>
+                        <input type="text" id="inputPassword">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="container-background">
+            <img src="@/assets/global/background-form.png" alt="">
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.main-container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.container-auth {
+    width: 100%;
+}
+
+.container-form {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+
+.container-logo {
+    padding: 40px 50px;
+}
+
+.title-form {
+    font-family: 'Poppins', serif;
+    text-align: center;
+    background-image: linear-gradient(180deg, #FE5C2B 0%, #98371A 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+form {
+    width: 400px;
+}
+</style>
