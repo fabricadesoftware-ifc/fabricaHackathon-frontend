f5ec191cc637ff4081547d8bcf7171c748740187
AnthonyLoche <anthonylocheifc@gmail.com>
2024-09-14 20:30:28 -0300

    FEAT(Home): Creating homeview, with the composables to open or no registrations [Refs: #38]

--- src/components/home/CardsComp.vue
+++ src/components/home/CardsComp.vue
@@ -1,43 +1,39 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import { prepareEditions } from "@/composables/edition/editionUtils";
-import { useEditionStore } from "@/stores/edition";
-import { useClassInfoStore } from "@/stores/classInfo";
-import CardEditionHome from "../global/card/CardEditionHome.vue";
+import { reactive } from 'vue';
+import CardEditionHome from '../global/cards/CardEditionHome.vue';
 
-const editionStore = useEditionStore();
-const classesInfoStore = useClassInfoStore();
-const formatEditions = ref([]);
-
-
-onMounted(async () => {
-  await editionStore.getEditions();
-  await classesInfoStore.getClassesInfo();
-  formatEditions.value = prepareEditions(editionStore.editions, classesInfoStore.classesInfo).slice(0, 3);
+const info = reactive({
+    title: "NÃO HÁ MANEIRA MELHOR DE APRENDER",
+    description: "Você pode aprender muito mais do que imagina, participe do hackaton e veja o que pode aprender",
+    route: '/',
+    img: "https://www.portaldoholanda.com.br/sites/default/files/imagecache/portal2014_fotonoticiagrande/portaldoholanda-626973-imagem-foto-amazonas.jpg",
+    year: "2024"
 });
-
 </script>
 
 <template>
-  <article class="cardsComp">
-    <CardEditionHome v-for="edition in formatEditions" :key="edition.year" :object="edition" />
-  </article>
+    <article class="cardsComp">
+        <CardEditionHome :object="info" />
+        <CardEditionHome :object="info" />
+        <CardEditionHome :object="info" />
+    </article>
 </template>
 
 <style scoped>
 .cardsComp {
-  width: 100%;
-  display: flex;
-  gap: 2rem;
-  height: 50vh;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    gap: 2rem;
+    height: 50vh;
 }
 
 .cardsComp>* {
-  flex: 1;
-  transition: flex-grow 0.3s ease-in-out;
+    flex: 1;
+    transition: flex-grow 0.3s ease-in-out;
 }
 
 .cardsComp>*:hover {
-  flex-grow: 1.5;
+    flex-grow: 1.5;
 }
 </style>
