92a3c864ee1c4b38fa3f33a9eff8e019a40ad997
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-24 16:41:42 -0300

    (HEADER): Header components [Refs: #31]

--- src/components/global/button/HeaderButton.vue
+++ src/components/global/button/HeaderButton.vue
@@ -1,59 +1,70 @@
 <script setup>
+import ArrowTopRight from "vue-material-design-icons/ArrowTopRight.vue";
+
 defineProps({
-    text: String
-})
+	text: String,
+});
 </script>
 
 <template>
-    <div class="button-container">
-        <button>
-            {{ text }}
-        </button>
-        <router-link>></router-link>
-    </div>
+	<div class="button-container">
+		<button>
+			{{ text }}
+		</button>
+		<router-link><ArrowTopRight /></router-link>
+	</div>
 </template>
 
 <style scoped>
 .button-container {
-    display: flex;
-    align-items: center;
+	display: flex;
+	align-items: center;
 }
 
 button {
-    background: transparent;
-    border: 2px solid #fff;
-    padding: 1rem 2rem;
-    border-radius: 27px;
-    color: #fff;
-    display: flex;
-    align-items: center;
-    position: relative;
-    transition: .5s ease-in-out;
-    cursor: pointer;
-}
-
-button:hover {
-    background: #fff;
-    color: #000;
+	background: transparent;
+	border: 1px solid #fff;
+	padding: 0.7rem 2rem;
+	border-radius: 27px;
+	color: #fff;
+	font-weight: 600;
+	display: flex;
+	align-items: center;
+	position: relative;
+	transition: 0.5s ease-in-out;
+	cursor: pointer;
 }
 
 a {
-    text-decoration: none;
-    width: 53px;
-    height: 53px;
-    background: #fff;
-    border-radius: 50%;
-    color: #000;
-    margin-left: -10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: .5s ease-in-out;
+	text-decoration: none;
+	width: 50px;
+	height: 50px;
+	background: #fff;
+	border-radius: 50%;
+	color: #000;
+	margin-left: -10px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	transition: 0.5s ease-in-out;
 }
 
-a:hover {
-    background: transparent;
-    color: #fff;
-    border: 2px solid #fff;
+.button-container:hover {
+	button {
+		background: #fff;
+		color: #000;
+	}
+
+	a {
+		background: black;
+		color: #fff;
+		border: 2px solid #fff;
+		z-index: 3;
+	}
+}
+
+span {
+	display: flex;
+	justify-content: center;
 }
 </style>
