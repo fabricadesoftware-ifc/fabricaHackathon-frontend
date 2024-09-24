77b36e46b1b29f14beedbbafaff03103cb2573bd
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-24 16:43:29 -0300

    (CARDS): Cards components [Refs: #31]

--- src/components/global/cards/GradientBall.vue
+++ src/components/global/cards/GradientBall.vue
@@ -1,32 +1,31 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <div class="all">
-        <div class="alignBalls">
-        <span class="one"></span>
-        <span class="two"></span>
-        <span class="three"></span>
-        <span class="four"></span>
-        <span class="five"></span>
-        <span class="six"></span>
-    </div>
-    </div>
+  <div class="all">
+	<div class="alignBalls">
+	  <span class="one"></span>
+	  <span class="two"></span>
+	  <span class="three"></span>
+	  <span class="four"></span>
+	  <span class="five"></span>
+	  <span class="six"></span>
+	</div>
+  </div>
 </template>
 
 <style scoped>
 .all {
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: start;
-    height: 100%;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: start;
+  height: 100%;
 }
 
 .alignBalls {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .one,
@@ -35,90 +34,91 @@
 .four,
 .five,
 .six {
-    width: 3rem;
-    height: 3rem;
-    border-radius: 50%;
-    border: 1px solid transparent;
-    position: relative;
-    overflow: hidden;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  border: 1px solid transparent;
+  position: relative;
+  overflow: hidden;
 }
 
 .two {
-    margin-left: -20px;
+  margin-left: -20px;
 }
 
 .three {
-    margin-left: -20px;
+  margin-left: -20px;
 }
 
 .one,
 .two,
 .three {
-    border: 1px solid white;
+  border: 1px solid white;
 }
 
 .four {
-    margin-left: -20px;
-    background: #FFFFFF3D;
-    padding: -1px;
-
+  margin-left: -20px;
+  background: #ffffff3d;
+  padding: -1px;
 }
 
 .four::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    margin: -4px;
-    border-radius: 50%;
-    padding: 5px;
-    background: radial-gradient(97.57% 210.75% at 0.9% 2.98%, rgba(216, 216, 216, 0) 0%, #D8D8D8 100%);
-    -webkit-mask:
-        linear-gradient(#fff 0 0) content-box,
-        linear-gradient(#fff 0 0);
-    mask:
-        linear-gradient(#fff 0 0) content-box,
-        linear-gradient(#fff 0 0);
-    mask-composite: exclude;
-    -webkit-mask-composite: destination-out;
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: -4px;
+  border-radius: 50%;
+  padding: 5px;
+  background: radial-gradient(
+	97.57% 210.75% at 0.9% 2.98%,
+	rgba(216, 216, 216, 0) 0%,
+	#d8d8d8 100%
+  );
+  -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+  mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+  mask-composite: exclude;
+  -webkit-mask-composite: destination-out;
 }
 
 .five {
-    margin-left: -20px;
-    background: #FFFFFF12;
-
+  margin-left: -20px;
+  background: #ffffff12;
 }
 
 .four::before,
 .five::before,
 .six::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    margin: -4px;
-    border-radius: 50%;
-    padding: 5px;
-    background: radial-gradient(97.57% 210.75% at 0.9% 2.98%, rgba(216, 216, 216, 0) 0%, #D8D8D8 100%);
-    border-image-source: linear-gradient(114.55deg, rgba(255, 255, 255, 0.52) 2.13%, rgba(255, 255, 255, 0) 98.14%);
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: -4px;
+  border-radius: 50%;
+  padding: 5px;
+  background: radial-gradient(
+	97.57% 210.75% at 0.9% 2.98%,
+	rgba(216, 216, 216, 0) 0%,
+	#d8d8d8 100%
+  );
+  border-image-source: linear-gradient(
+	114.55deg,
+	rgba(255, 255, 255, 0.52) 2.13%,
+	rgba(255, 255, 255, 0) 98.14%
+  );
 
-    -webkit-mask:
-        linear-gradient(#fff 0 0) content-box,
-        linear-gradient(#fff 0 0);
-    mask:
-        linear-gradient(#fff 0 0) content-box,
-        linear-gradient(#fff 0 0);
-    mask-composite: exclude;
-    -webkit-mask-composite: destination-out;
+  -webkit-mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+  mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+  mask-composite: exclude;
+  -webkit-mask-composite: destination-out;
 }
 
-
 .six {
-    margin-left: -20px;
-    background: #00000050;
+  margin-left: -20px;
+  background: #00000050;
 }
 </style>
