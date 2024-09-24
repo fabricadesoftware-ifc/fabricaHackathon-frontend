81095b2704d216aa6a5966e8e3bee75121ac9f42
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-24 16:44:37 -0300

    (FOOTER): Footer components [Refs: #31]

--- src/components/global/footer/FooterComp.vue
+++ src/components/global/footer/FooterComp.vue
@@ -1,39 +1,36 @@
 <script setup>
-import SocialsComp from './SocialsComp.vue';
-import LinksComp from './LinksComp.vue';
+import SocialsComp from "./SocialsComp.vue";
+import LinksComp from "./LinksComp.vue";
 </script>
 
-
 <template>
-    <footer>
-        <div class="logo">
-            <img src="/public/logoHackaton.png" alt="">
-        </div>
-        <SocialsComp />
-        <LinksComp />
-    </footer>
+  <footer>
+	<div class="logo">
+	  <img src="/public/logoHackaton.png" alt="" />
+	</div>
+	<SocialsComp />
+	<LinksComp />
+  </footer>
 </template>
 
 <style scoped>
 footer {
-    background: #131316;
-    width: 100%;
-    padding: 1rem;
-    height: 430px;
-    display: grid;
-    grid-template-columns: 1fr 0.5fr 1fr;
+  background: #131316;
+  width: 100%;
+  padding: 5rem 1rem 2rem 1rem;
+  height: 430px;
+  display: grid;
+  grid-template-columns: 1fr 0.5fr 1fr;
 }
 
 img {
-    width: 40%;
-    margin: 0 auto;
+  width: 40%;
+  margin: 0 auto;
 }
 
 .logo {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
 }
-
 </style>
