9dfde9dfde5196a011672fd1a3f1db74311bd950
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-24 09:23:54 -0300

    REFACTOR(hero): Hero components [Refs: #31]

--- src/components/home/HeroCompHome.vue
+++ src/components/home/HeroCompHome.vue
@@ -1,82 +1,66 @@
 <script setup>
-import RoundCardCode from '../global/cards/RoundCardCode.vue';
-import SquareCardBox from '../global/cards/SquareCardBox.vue';
-import BallCard from '../global/cards/BallCard.vue';
+import RoundCardCode from "../global/cards/RoundCardCode.vue";
+import SquareCardBox from "../global/cards/SquareCardBox.vue";
+import BallCard from "../global/cards/BallCard.vue";
 
 import CubeOutline from "vue-material-design-icons/CubeOutline.vue";
 import ConsoleLine from "vue-material-design-icons/ConsoleLine.vue";
 
-import { useScreen } from '@/composables/hero';
+import { useScreen } from "@/composables/hero";
 const { hero } = useScreen();
-
 </script>
 
 <template>
-    <section>
-        <BallCard y="50%" x="20%" />
-        <BallCard y="70%" x="90%" />
-        <BallCard y="75%" x="60%" />
-        <BallCard y="20%" x="75%" />
-        <div class="cards1">
-            <RoundCardCode />
-            <span></span>
-            <SquareCardBox :component="CubeOutline" />
-        </div>
-        <div class="text">
-
-            <component :is="hero" />
-        </div>
-        <div class="cards2">
-            <span></span>
-            <SquareCardBox :component="ConsoleLine" />
-            <span></span>
-        </div>
-    </section>
+  <section>
+    <BallCard y="50%" x="20%" />
+    <BallCard y="70%" x="90%" />
+    <BallCard y="75%" x="60%" />
+    <BallCard y="20%" x="75%" />
+    <div class="cards1">
+      <RoundCardCode />
+      <SquareCardBox :component="CubeOutline" />
+    </div>
+    <div class="text">
+      <component :is="hero" />
+    </div>
+    <div class="card2">
+      <SquareCardBox :component="ConsoleLine" />
+    </div>
+  </section>
 </template>
 
 <style scoped>
 section {
-    width: 100%;
-    display: grid;
-    grid-template-columns: 1.5fr 4fr 1.5fr;
-    height: 75vh;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1.5fr 4fr 1.5fr;
+  height: 75vh;
 }
 
 .text {
-    display: flex;
-    justify-content: center;
-    /* Centraliza horizontalmente */
-    align-items: center;
-    /* Centraliza verticalmente */
-    width: 100%;
-    /* Garante que o div ocupe toda a largura disponível */
-    height: 100%;
-    /* Garante que o div ocupe toda a altura disponível */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
 }
 
 .cards1 {
-    display: grid;
-    grid-template-rows: 2fr 2fr 2fr;
-    gap: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding-bottom: 20%;
 }
 
-.cards2 {
-    display: grid;
-    grid-template-rows: 2fr 2fr 2fr;
-    gap: 1rem;
+.card2 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.cards1>*:first-child {
-    justify-self: end;
-    align-self: end;
-}
-
-.cards1>*:last-child {
-    justify-self: center;
-}
-
-.cards2>*:nth-child(2) {
-    justify-self: center;
-    margin-top: 15%;
+.cards1 > *:first-child {
+  justify-self: end;
+  align-self: end;
 }
 </style>
