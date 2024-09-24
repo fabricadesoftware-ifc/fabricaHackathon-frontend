46819b82a361822b3e14b562df4355aed0ab16cd
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-09-24 09:23:54 -0300

    REFACTOR(hero): Hero components [Refs: #31]

--- src/components/global/button/ButtonHeroOpen.vue
+++ src/components/global/button/ButtonHeroOpen.vue
@@ -1,91 +1,81 @@
 <script setup>
-import ArrowRight from 'vue-material-design-icons/ArrowRight.vue'
+import ArrowRight from "vue-material-design-icons/ArrowRight.vue";
 </script>
 
 <template>
-    <div class="mainButton">
-        <div class="card">
-            <div class="text">
-                Acessar
-            </div>
-            <div class="codeBox">
-                <div class="code">
-                    <ArrowRight />
-                </div>
-            </div>
+  <div class="mainButton">
+    <div class="card">
+      <div class="text">Acessar</div>
+      <div class="codeBox">
+        <div class="code">
+          <ArrowRight />
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .mainButton {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    border-radius: 50px;
-    color: #FFFFFF;
-    transition: 0.3s ease-in-out;
-    position: relative;
+  width: 100%;
+  border-radius: 50px;
+  color: #ffffff;
+  transition: 0.3s ease-in-out;
+  position: relative;
 }
 
 .card {
-    width: 160px;
-    background: transparent;
-    border: 1px solid #FFFFFF;
-    height: 60px;
-    border-radius: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding-left: 1.5rem;
-    gap: .5rem;
-    position: relative;
-    transition: background-color 0.3s ease-in-out;
+  width: 160px;
+  background: transparent;
+  border: 1px solid #ffffff;
+  height: 60px;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 1.5rem;
+  gap: 0.5rem;
+  position: relative;
+  transition: background-color 0.3s ease-in-out;
 }
 
 .codeBox {
-    width: 50%;
-    position: relative;
-    z-index: 100;
+  position: relative;
+  background-color: black;
+  border-radius: 50%;
 }
 
 .code {
-    width: 60px;
-    height: 60px;
-    background: radial-gradient(97.57% 210.75% at 0.9% 2.98%, rgba(255, 255, 255, 0.58) 30.49%, rgba(255, 255, 255, 0) 100%);
-    border-radius: 50%;
-    color: #FFFFFF;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid #FFFFFF;
-    font-weight: 1000;
+  width: 60px;
+  height: 60px;
+  background: radial-gradient(
+    97.57% 210.75% at 0.9% 2.98%,
+    rgba(255, 255, 255, 0.58) 30.49%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  border-radius: 50%;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ffffff;
+  font-weight: 1000;
 }
 
 .text {
-    font-size: 1rem;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    height: 50px;
-    font-weight: 400;
-    z-index: 1;
+  width: 100%;
+  display: flex;
+  align-items: center;
 }
 
 span {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
-.mainButton:hover {
-    cursor: pointer;
-    background-color: white;
-    color: black;
-}
-
-.mainButton:hover .card .code {
-    z-index: 10;
-
+.card:hover {
+  cursor: pointer;
+  background-color: white;
+  color: black;
 }
 </style>
--- src/components/home/OpenHero.vue
+++ src/components/home/OpenHero.vue
@@ -1,102 +1,101 @@
 <script setup>
-import GradientBall from '../global/cards/GradientBall.vue';
-import ButtonHeroOpen from '../global/buttons/ButtonHeroOpen.vue';
+import GradientBall from "../global/cards/GradientBall.vue";
+import ButtonHeroOpen from "../global/buttons/ButtonHeroOpen.vue";
 </script>
 
 <template>
-    <div class="openHero">
-        <div class="row1">
-            <p class="gradient">INSCREVA-SE</p>
-            <div class="gradientBall">
-                <GradientBall />
-            </div>
-        </div>
-        <div class="row2">
-            <span>
-                <ButtonHeroOpen />
-            </span>
-            <p class="gradient">
-                <i>AGORA</i>
-                PARA
-            </p>
-        </div>
-        <div class="row3">
-            <p class="gradient">O HACKATON!</p>
-        </div>
-    </div>
+  <div class="openHero">
+	<div class="row1">
+	  <p class="gradient">INSCREVA-SE</p>
+	  <div class="gradientBall">
+		<GradientBall />
+	  </div>
+	</div>
+	<div class="row2">
+	  <span>
+		<ButtonHeroOpen />
+	  </span>
+	  <p class="gradient">
+		<i>AGORA</i>
+		PARA
+	  </p>
+	</div>
+	<div class="row3">
+	  <p class="gradient">O HACKATON!</p>
+	</div>
+  </div>
 </template>
 
 <style scoped>
 .openHero {
-    width: 90%;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    border-radius: 15px;
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 15px;
 }
 
 .row1,
 .row2,
 .row3 {
-    width: 95%;
-    display: flex;
-    align-items: center;
+  width: 95%;
+  display: flex;
+  align-items: center;
 }
 
-
 .row2 {
-    gap: 5rem;
+  gap: 5rem;
 }
 
 .gradientBall {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
 }
 
 .row2 span {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 15%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15%;
 }
 
 .gradient {
-    background: linear-gradient(85.58deg, rgba(254, 92, 43, 0.38) 8%, #FE5C2B 139.33%);
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-    font-size: 5vw;
-    line-height: 1.2;
-    font-weight: 400;
-    text-align: center;
+  background: linear-gradient(85.58deg, rgba(254, 92, 43, 0.38) 8%, #fe5c2b 139.33%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 5vw;
+  line-height: 1.2;
+  font-weight: 400;
+  text-align: center;
 }
 
 @media (max-width: 1200px) {
-    .gradient {
-        font-size: 6vw;
-    }
+  .gradient {
+	font-size: 6vw;
+  }
 }
 
 @media (max-width: 768px) {
-    .gradient {
-        font-size: 8vw;
-    }
+  .gradient {
+	font-size: 8vw;
+  }
 
-    .row2 {
-        gap: 2rem;
-    }
+  .row2 {
+	gap: 2rem;
+  }
 }
 
 @media (max-width: 480px) {
-    .gradient {
-        font-size: 10vw;
-    }
+  .gradient {
+	font-size: 10vw;
+  }
 
-    .row2 span {
-        width: 30%;
-    }
+  .row2 span {
+	width: 30%;
+  }
 }
 </style>
