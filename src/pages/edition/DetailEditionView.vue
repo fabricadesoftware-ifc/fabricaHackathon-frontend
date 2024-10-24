b03929bd46f8ee130f77a4c9983f81f8eaa67c9b
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-10-23 22:07:46 -0300

    CHORE: Finish services of detailsEdition #42

--- src/pages/edition/DetailEditionView.vue
+++ src/pages/edition/DetailEditionView.vue
@@ -1,6 +1,17 @@
 <script setup>
 import HeroDetailComp from '@/components/edition/HeroDetailComp.vue';
 import SectionDetailComp from '@/components/edition/SectionDetailComp.vue';
+import { onMounted } from 'vue';
+import { useDetailEditionStore } from '@/stores/detailEdition';
+import { useRoute } from 'vue-router';
+
+const useDetailEdition = useDetailEditionStore();
+const route = useRoute();
+
+onMounted(async() => {
+    useDetailEdition.editionId = route.params.edition;
+    await useDetailEdition.populateRefs();
+})
 
 </script>
 
