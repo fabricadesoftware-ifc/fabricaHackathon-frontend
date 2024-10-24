1e6898d9289f09edf5c7f7899f810a865042ace2
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-10-23 22:07:46 -0300

    CHORE: Finish services of detailsEdition #42

--- src/services/detailEdition.js
+++ src/services/detailEdition.js
@@ -2,19 +2,48 @@ import { api } from "@/plugins/axios";
 
 
 class DetailEditionService {
+    #idcategories = [];
+    
     async orderByWinningTeams(id) {
         const { data } = await api.get(`/rankings?edition_id=${id}&classification=1`);
         return data;
     }
-    async orderBySalesTeams(id) {
+    async orderBySalesTeams(id, nameCategory) {
+        const category = await this.getCategoryId(nameCategory);
+        const { data } = await api.get(`/rankings?edition_id=${id}&category=${category}`);
+        return data;
+    }
+    async orderByServicesTeams(id, nameCategory) {
+        const category = await this.getCategoryId(nameCategory);
+        const { data } = await api.get(`/rankings?edition_id=${id}&category=${category}`);
+        return data;
+    }
+    async orderByRentalsTeams(id, nameCategory) {
+        const category = await this.getCategoryId(nameCategory);
+        const { data } = await api.get(`/rankings?edition_id=${id}&category=${category}`);
+        return data;
+    }
+    async orderByUncategorized(id) {
+        let { data } = await api.get(`/rankings?edition_id=${id}`);
+        data = data.filter(team => {
+            return(
+            team.team.category != this.#idcategories[0] &&
+            team.team.category != this.#idcategories[1] &&
+            team.team.category != this.#idcategories[2]
+            )
+        });
+        return data;
+    }
+    async getCategoryId(nameCategory) {
         const categories = await api.get('/categories/');
         let category = undefined;
         for (let c = 0; c < categories.data.length; c++) {
-            if (categories.data[c].name === 'Sales') {
+            if (categories.data[c].name === nameCategory) {
                 category = categories.data[c].id;
             }
         }
-        const { data } = await api.get(`/rankings?edition_id=${id}&category=${category}`);
+        this.#idcategories.push(category);
+        return category;
     }
 };
 
