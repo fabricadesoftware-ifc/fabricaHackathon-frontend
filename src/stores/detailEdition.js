718feb9dfd638112b3b6782b622688a6b38ac38b
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-10-24 22:39:24 -0300

    CHORE: Add services, rental and no categories in SectionDetailComp #42

--- src/stores/detailEdition.js
+++ src/stores/detailEdition.js
@@ -16,7 +16,6 @@ export const useDetailEditionStore = defineStore('detailEdition', () => {
     const getAllTeams = async(idEdition) => {
         const data = await DetailEditionService.getAllTeams(idEdition);
         countTeams.value = data.length;
-
         populateRefs(data);
     };
 
@@ -26,19 +25,16 @@ export const useDetailEditionStore = defineStore('detailEdition', () => {
     const orderBySalesTeams = (teams = []) => {
         const index = useCategory.getIdCategoryByName('Category 8');
         console.log(index);
-
         salesTeams.value = DetailEditionService.orderBySalesTeams(index, teams)
         categoriesUsed.push(index);
     };
     const orderByServicesTeams = (teams = []) => {
         const index = useCategory.getIdCategoryByName('Category 8');
-
         servicesTeam.value = DetailEditionService.orderByServicesTeams(index, teams);
         categoriesUsed.push(index);
     }
     const orderByRentalsTeams = (teams = []) => {
         const index = useCategory.getIdCategoryByName('Category 8');
-
         rentalsTeams.value = DetailEditionService.orderByRentalsTeams(index, teams);
         categoriesUsed.push(index);
     }
