9d2cbb9ff279b50cc2e7634988e1bf356e3fc60c
jonatassilvaperaza@gmail.com <jonatassilvaperaza@gmail.com>
2024-11-13 19:44:54 -0300

    FEAT: Refatorar ProjectFormComp para usar store de projetos e atualizar campos do formulário #59

--- src/components/team/project/ProjectFormComp.vue
+++ src/components/team/project/ProjectFormComp.vue
@@ -1,43 +1,16 @@
 <script setup>
-import { reactive, onMounted, ref } from 'vue'
-import { useTeamStore } from '@/stores/team'
-import { useAuthStore } from '@/stores/auth'
-import { useRoute } from 'vue-router'
+import { reactive } from 'vue'
+import { useProjectStore } from '@/stores/project'
 import GradientInput from '@/components/global/input/GradientInput.vue'
 import OrangeButton from '@/components/global/button/OrangeButton.vue'
 
-const teamStore = useTeamStore()
-const authStore = useAuthStore()
-
-const route = useRoute()
-
-const currentTeam = ref(null)
-
-function findTeamByStudentIdAndEdition(studentId, editionId) {
-    return teamStore.teams.find(team => 
-        team.edition === editionId && team.students.includes(studentId)
-    )
-}
+const projectStore = useProjectStore()
 
 const dados = reactive({
     id: 0,  
-    project_name: '',
+    name: '',
     deploy_link: '',
     repository_link: '',
-})
-
-onMounted(async () => {
-    await teamStore.getTeams()
-
-    const editionId = parseInt(route.params.edition)
-
-    currentTeam.value = findTeamByStudentIdAndEdition(authStore.student_profile_data.id, editionId)
-
-    if (currentTeam.value) {
-        dados.id = currentTeam.value.id
-    }
-
-    console.log(dados)  
 })
 
 </script>
@@ -50,11 +23,11 @@ onMounted(async () => {
         <div class="form">
             <h1 style="text-align: center" class="gradientOrange">Cadastrar Projeto</h1>
             <form @submit.prevent>
-                <GradientInput v-model:text="dados.project_name" label="Nome do Projeto" />
+                <GradientInput v-model:text="dados.name" label="Nome do Projeto" />
                 <GradientInput v-model:text="dados.deploy_link" label="Deploy Link" />
                 <GradientInput v-model:text="dados.repository_link" label="Repository Link" />
 
-                <OrangeButton label="Enviar" @click="teamStore.updateTeam(dados)" />
+                <OrangeButton label="Enviar" @click="projectStore.createProject(dados)" />
             </form>
         </div>
     </section>
